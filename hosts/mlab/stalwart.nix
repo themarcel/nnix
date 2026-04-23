676122a6224f905df0972b6c243522c3bfca0efd
marcelarie <googlillo@gmail.com>
2026-04-23 08:13:56 +0200

    fix stalwart

--- hosts/mlab/stalwart.nix
+++ hosts/mlab/stalwart.nix
@@ -17,6 +17,8 @@
       server.hostname = lib.removePrefix "https://" services.stalwart.href;
       server.trusted-proxies = ["127.0.0.1" "::1"];
 
+      http.url = "'${services.stalwart.href}'";
+
       authentication.fallback-admin = {
         user = "admin";
         secret = "%{file:${config.sops.secrets.stalwart_admin_pass.path}}%";
@@ -26,19 +28,13 @@
         jmap = {
           bind = ["127.0.0.1:${toString services.stalwart.port}"];
           protocol = "http";
+          url = services.stalwart.href;
         };
         management = {
           bind = ["127.0.0.1:8087"];
           protocol = "http";
           url = services.stalwartadmin.href;
         };
-      };
-
-      jmap = {
-        url = "${services.stalwart.href}/jmap";
-        download-url = "${services.stalwart.href}/jmap/download/{accountId}/{blobId}/{name}?accept={type}";
-        upload-url = "${services.stalwart.href}/jmap/upload/{accountId}/";
-        event-source-url = "${services.stalwart.href}/jmap/eventsource/?types={types}&closeafter={closeafter}&ping={ping}";
       };
 
       storage = {
